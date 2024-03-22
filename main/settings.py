89a7084e73a5156cf66324a1aaaa1f3e741b0e1b
User <kingradomir@gmail.com>
2024-03-22 11:07:58 +0600

    'qwerty'

--- main/settings.py
+++ main/settings.py
@@ -108,6 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [BASE_DIR / 'static']
