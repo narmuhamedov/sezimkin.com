757e3e42b8d6ea6d06fd079bf700c5708758db6a
User <kingradomir@gmail.com>
2024-11-06 15:47:22 +0600

    'endproject'

--- main/settings.py
+++ main/settings.py
@@ -19,12 +19,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['radomirnarmukhamedov123.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    "jazzmine",
     "admin_interface",
     "colorfield",
     "django.contrib.admin",
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
